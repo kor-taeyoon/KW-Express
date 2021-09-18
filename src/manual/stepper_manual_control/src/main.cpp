a38a02a34c4a743bdddc1ba6c5a8d55ca415156e
kor-taeyoon <dbsl0512@gmail.com>
2021-09-18 22:42:58 +0900

    manual controlling firmware complete

--- src/manual/stepper_manual_control/src/main.cpp
+++ src/manual/stepper_manual_control/src/main.cpp
@@ -1,15 +1,51 @@
 #include <Arduino.h>
 #include <AccelStepper.h>
 
+AccelStepper stepper_l(AccelStepper::DRIVER, 2, 3);
+AccelStepper stepper_r(AccelStepper::DRIVER, 4, 5);
+
 void setup() {
-    // put your setup code here, to run once:
-    pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(9600);
+    
+    pinMode(2, OUTPUT);
+    pinMode(3, OUTPUT);
+    pinMode(4, OUTPUT);
+    pinMode(5, OUTPUT);
+    stepper_l.setMaxSpeed(8000);
+    stepper_l.setAcceleration(2000);
+    stepper_r.setMaxSpeed(8000);
+    stepper_r.setAcceleration(2000);
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(1000);
+    stepper_l.run();
+    stepper_r.run();
+    if(Serial.available()){
+        char tmp = Serial.read();
+        if(tmp == 'w'){     // forward
+            Serial.println("forward");
+            stepper_l.move(1000000000);
+            stepper_r.move(-1000000000);
+        }
+        else if(tmp == 'x'){     // backward
+            Serial.println("backward");
+            stepper_l.move(-1000000000);
+            stepper_r.move(1000000000);
+        }
+        else if(tmp == 'a'){     // leftward
+            Serial.println("left");
+            stepper_l.move(-1000000000);
+            stepper_r.move(-1000000000);
+        }
+        else if(tmp == 'd'){     // rightward
+            Serial.println("right");
+            stepper_l.move(1000000000);
+            stepper_r.moveTo(1000000000);
+        }
+        else if(tmp == 's'){
+            Serial.println("stop");
+            stepper_l.stop();
+            stepper_r.stop();
+        }
+    }
 }
