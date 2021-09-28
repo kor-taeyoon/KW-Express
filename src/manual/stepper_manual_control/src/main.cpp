d9b7077c8d063a2f11f7b567c8729fd816538f42
kor-taeyoon <dbsl0512@gmail.com>
2021-09-29 00:28:39 +0900

    stm32f767 firmware update

--- src/manual/stepper_manual_control/src/main.cpp
+++ src/manual/stepper_manual_control/src/main.cpp
@@ -1,20 +1,38 @@
+#define L_STEP_PIN 2
+#define L_DIR_PIN 3
+#define R_STEP_PIN 4
+#define R_DIR_PIN 5
+#define L_EN_PIN 6
+#define R_EN_PIN 7
+
 #include <Arduino.h>
 #include <AccelStepper.h>
 
-AccelStepper stepper_l(AccelStepper::DRIVER, 2, 3);
-AccelStepper stepper_r(AccelStepper::DRIVER, 4, 5);
+AccelStepper stepper_l(AccelStepper::DRIVER, L_STEP_PIN, L_DIR_PIN);
+AccelStepper stepper_r(AccelStepper::DRIVER, R_STEP_PIN, R_DIR_PIN);
+
+int MaxSpeed = 8000;
+int Accel = 8000;
 
 void setup() {
     Serial.begin(9600);
     
-    pinMode(2, OUTPUT);
-    pinMode(3, OUTPUT);
-    pinMode(4, OUTPUT);
-    pinMode(5, OUTPUT);
-    stepper_l.setMaxSpeed(8000);
-    stepper_l.setAcceleration(2000);
-    stepper_r.setMaxSpeed(8000);
-    stepper_r.setAcceleration(2000);
+    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(L_STEP_PIN, OUTPUT);
+    pinMode(L_DIR_PIN, OUTPUT);
+    pinMode(R_STEP_PIN, OUTPUT);
+    pinMode(R_DIR_PIN, OUTPUT);
+    pinMode(L_EN_PIN, OUTPUT);
+    pinMode(R_EN_PIN, OUTPUT);
+    
+    stepper_l.setMaxSpeed(MaxSpeed);
+    stepper_l.setAcceleration(Accel);
+    stepper_r.setMaxSpeed(MaxSpeed);
+    stepper_r.setAcceleration(Accel);
+
+    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(L_EN_PIN, LOW);
+    digitalWrite(R_EN_PIN, LOW);
 }
 
 void loop() {
@@ -23,29 +41,44 @@ void loop() {
     if(Serial.available()){
         char tmp = Serial.read();
         if(tmp == 'w'){     // forward
-            Serial.println("forward");
+            //Serial.println("forward");
+            digitalWrite(LED_BUILTIN, HIGH);
             stepper_l.move(1000000000);
             stepper_r.move(-1000000000);
         }
         else if(tmp == 'x'){     // backward
-            Serial.println("backward");
+            //Serial.println("backward");
+            digitalWrite(LED_BUILTIN, HIGH);
             stepper_l.move(-1000000000);
             stepper_r.move(1000000000);
         }
         else if(tmp == 'a'){     // leftward
-            Serial.println("left");
+            //Serial.println("left");
+            digitalWrite(LED_BUILTIN, HIGH);
             stepper_l.move(-1000000000);
             stepper_r.move(-1000000000);
         }
         else if(tmp == 'd'){     // rightward
-            Serial.println("right");
+            //Serial.println("right");
+            digitalWrite(LED_BUILTIN, HIGH);
             stepper_l.move(1000000000);
-            stepper_r.moveTo(1000000000);
+            stepper_r.move(1000000000);
         }
         else if(tmp == 's'){
-            Serial.println("stop");
+            //Serial.println("stop");
+            digitalWrite(LED_BUILTIN, LOW);
             stepper_l.stop();
             stepper_r.stop();
+        }
+        else if(tmp == 'z'){
+            MaxSpeed-=1000;
+            stepper_l.setMaxSpeed(MaxSpeed);
+            stepper_r.setMaxSpeed(MaxSpeed);
+        }
+        else if(tmp == 'c'){
+            MaxSpeed+=1000;
+            stepper_l.setMaxSpeed(MaxSpeed);
+            stepper_r.setMaxSpeed(MaxSpeed);
         }
     }
 }
